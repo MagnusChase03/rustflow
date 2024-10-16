c2aaa26df698425134b2f6cbcee951305f4137b8
Zachary <zachness2003@gmail.com>
2024-10-16 12:04:25 -0500

    XOR problem, and random weights

--- src/layer.rs
+++ src/layer.rs
@@ -1,3 +1,6 @@
+use rand;
+use rand::Rng;
+
 use std::io;
 use super::activation_function;
 
@@ -20,13 +23,19 @@ impl Layer {
         output_size: usize,
         activation_function: Box<dyn activation_function::ActivationFunction>
     ) -> Self {
+        let mut rng = rand::thread_rng();
+        let mut weights: Vec<f64> = Vec::new();
+        for _ in 0..output_size * input_size {
+            weights.push(rng.gen());
+        }
+
         return Layer{
             input_size,
             output_size,
             inputs: vec![0.0; input_size],
             outputs: vec![0.0; output_size],
             cache: vec![0.0; output_size],
-            weights: vec![0.01; input_size * output_size],
+            weights,
             bias: vec![0.0; output_size],
             activation_function
         }; 
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,48 @@ fn main() {
     let shape: Vec<usize> = vec![3, 2, 2];
     let mut n = network::Network::new(&shape, sigmoid);
 
-    let inputs = vec![1.0, 2.0, 3.0];
-    let _ = n.forward(&inputs).unwrap();
-    println!("{:?}", &n.outputs);
+    let inputs = vec![
+        vec![-1.0, -1.0, -1.0],
+        vec![-1.0, -1.0,  1.0],
+        vec![-1.0,  1.0, -1.0],
+        vec![-1.0,  1.0,  1.0],
+        vec![ 1.0, -1.0, -1.0],
+        vec![ 1.0, -1.0,  1.0],
+        vec![ 1.0,  1.0, -1.0],
+        vec![ 1.0,  1.0,  1.0],
+    ];
+    let outputs = vec![
+        -1.0,
+         1.0,
+         1.0,
+        -1.0,
+         1.0,
+        -1.0,
+        -1.0,
+         1.0
+    ];
 
-    let errors = vec![-0.1, 0.1];
-    let _ = n.backward(&errors, 0.01).unwrap();
-    let _ = n.forward(&inputs).unwrap();
-    println!("{:?}", &n.outputs);
+    for _ in 0..1000 {
+        for i in 0..inputs.len() {
+            let _ = n.forward(&inputs[i]).unwrap();
+            let pred = &n.outputs;
+            if outputs[i] == 1.0 {
+                let errors = vec![-1.0 / pred[0], 1.0 / (1.0 - pred[1])];
+                let _ = n.backward(&errors, 0.01).unwrap();
+            } else {
+                let errors = vec![1.0 / (1.0 - pred[0]), -1.0 / pred[1]];
+                let _ = n.backward(&errors, 0.01).unwrap();
+            } 
+        }
+    }
+
+    for i in 0..inputs.len() {
+        let _ = n.forward(&inputs[i]).unwrap();
+        let pred = &n.outputs;
+        if pred[0] >= pred[1] {
+            println!("1.0");
+        } else {
+            println!("-1.0");
+        }
+    }
 }
