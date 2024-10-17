a6a068c6cdcdbe8ad14b38751023946eab8f172c
Zachary <zachness2003@gmail.com>
2024-10-17 12:56:06 -0500

    Better parameters

--- examples/main.rs
+++ examples/main.rs
@@ -29,14 +29,22 @@ fn main() {
     ];
 
     let mut model = rustflow::network::Network::new(layers, Box::new(rustflow::error_function::LogErr));
-    let _ = model.train(&inputs, &outputs, 10000, 0.01).unwrap();
+    let _ = model.train(&inputs, &outputs, 1000, 0.01).unwrap();
 
+    let mut correct = 0;
     for i in 0..inputs.len() {
         let pred = model.forward(&inputs[i]).unwrap();
         if pred[0] >= pred[1] {
             println!("1.0");
+            if outputs[i][0] == 1.0 {
+                correct += 1;
+            }
         } else {
             println!("0.0");
+            if outputs[i][1] == 1.0 {
+                correct += 1;
+            }
         }
     }
+    println!("Accuracy: {}", correct as f64 / outputs.len() as f64);
 }
